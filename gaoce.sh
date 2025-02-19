c359ade8ca505e41b5bb16fcc818088951bcf499
Gaoce8888 <gaoce51888@gmail.com>
2025-02-20 07:22:13 +0800

    Update gaoce.sh

--- gaoce.sh
+++ gaoce.sh
@@ -75,7 +75,8 @@ main_menu() {
     echo -e " 9. 推荐下载模型"
     echo -e " 10. webui"
     echo -e " 11. 显卡检测"
-    echo -e "10. 退出系统"
+    echo -e " 12. 查找文件"
+    echo -e "20. 退出系统"
     echo -e "╚════════════════════════════════════════╝"
     echo -e "${YELLOW}提示: 输入数字选择操作，输入q可随时退出${RESET}"
         
@@ -94,6 +95,7 @@ main_menu() {
         9) run_script "推荐下载模型" ./ai推荐下载.sh ;;
         10) run_script "webui" ./webUI.sh ;;
         11) run_script "显卡检测" ./显卡.sh ;;
+        12) run_script "查找文件" ./查找.sh ;;
         20|q|Q) safe_exit ;;
         *) show_error "无效输入！请输入1-10之间的数字" ;;
     esac
